1de9f1994181b7dd5bc1e153e7c07807454e670f
Ralf Barkow <ralf.barkow@me.com>
2025-09-06 12:52:26 +0200

    feat(app): restore local-first Browser.document entry and upstream Main;
    revive Feather icon picker; tolerate flags
    
    * AppMain: replace port entry with Browser.document using AM.Model/Msg;
      accept any JSON flags and pass through to Main.init or
      fall back to {"slug":"empty","stored":"{}"}; use Main.update/view and
      a temporary Sub.none subscriptions stub.
    * Main: reinstate full local-first application (toolbar, map, search,
      edit/measure, nav, hide/delete, moveTopicToMap, autoSize).
      Keep FedWikiPage handling by storing raw JSON.
    * IconMenuAPI: add and export viewIcon implemented via FeatherIcons Dict;
      remove placeholder text icon; use in icon list to
      restore the “Pick a Feather icon” flow.
    * Toolbar: resolve NoOp ambiguity by qualifying as AppModel.NoOp.
    * Config: bump date.
    * Index/build expectations unchanged; index.html now boots Elm.AppMain.

--- src/Main.elm
+++ src/Main.elm
@@ -1,96 +1,72 @@
-module Main exposing (Flags, Model, Msg, init, main, moveTopicToMap, subscriptions, update, view)
+module Main exposing (..)
 
-import AppModel as AM
+import AppModel exposing (..)
+import Boxing exposing (boxContainer, unboxContainer)
 import Browser
-import Feature.Connection.Channel as Channel
-import FedWiki
-import Html exposing (Html, div, h4, pre, text)
-import Html.Attributes exposing (style)
+import Browser.Dom as Dom
+import Compat.ModelAPI as ModelAPI exposing (addItemToMap)
+import Config exposing (..)
+import Dict
+import Html exposing (Attribute, br, div, text)
+import Html.Attributes exposing (id, style)
+import IconMenuAPI exposing (updateIconMenu, viewIconMenu)
 import Json.Decode as D
+import Json.Encode as E
+import MapAutoSize exposing (autoSize)
 import MapRenderer exposing (viewMap)
-import Model exposing (Id, Point)
-import ModelAPI exposing (activeMap)
+import Model exposing (..)
+import ModelAPI exposing (activeMap, createMap, createTopicIn, deleteItem, getMapId, getSingleSelection, getTopicProps, hasMap, hideItem, isItemInMap, select, setDisplayMode, setTopicPos, setTopicSize, updateMapRect, updateTopicInfo, updateTopicProps)
 import MouseAPI exposing (mouseHoverHandler, mouseSubs, updateMouse)
-import Storage exposing (modelDecoder)
-import String
+import SearchAPI exposing (updateSearch, viewResultMenu)
+import Storage exposing (modelDecoder, storeModel, storeModelWith)
+import String exposing (fromFloat, fromInt)
+import Task
+import UI.Toolbar exposing (viewToolbar)
+import Utils exposing (..)
 
 
 
--- Reuse canonical types
+-- MAIN
 
 
-type alias Model =
-    AM.Model
+main : Program E.Value Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = mouseSubs
+        }
 
 
-type alias Msg =
-    AM.Msg
-
-
-
--- Flags from JS bootstrap
-
-
-type alias Flags =
-    { slug : String
-    , stored : String
-    }
-
-
-
--- Lenient loader: try Storage.modelDecoder, else fall back to AM.default
-
-
-decodeOrDefault : String -> Model
-decodeOrDefault raw =
-    case D.decodeString modelDecoder raw of
-        Ok m ->
-            m
-
-        Err _ ->
-            AM.default
-
-
-
--- INIT
-
-
-init : Flags -> ( Model, Cmd Msg )
+init : E.Value -> ( Model, Cmd Msg )
 init flags =
-    let
-        m0 =
-            decodeOrDefault flags.stored
-    in
-    ( m0, Cmd.none )
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        AM.FedWikiPage raw ->
-            ( model
-                |> FedWiki.renderAsMonad raw
-                -- ensure a monad exists
-                |> (\m -> { m | fedWikiRaw = raw })
-              -- keep raw for the panel
-            , Cmd.none
-            )
+    ( case flags |> D.decodeValue (D.null True) of
+        Ok True ->
+            let
+                _ =
+                    info "init" "localStorage: empty"
+            in
+            default
 
         _ ->
-            ( model, Cmd.none )
+            case flags |> D.decodeValue modelDecoder of
+                Ok model ->
+                    let
+                        _ =
+                            info "init"
+                                ("localStorage: " ++ (model |> toString |> String.length |> fromInt) ++ " bytes")
+                    in
+                    model
 
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    mouseSubs model
+                Err e ->
+                    let
+                        _ =
+                            logError "init" "localStorage" e
+                    in
+                    default
+    , Cmd.none
+    )
 
 
 
@@ -99,95 +75,436 @@ subscriptions model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "DM6 Elm"
-    , body =
-        [ -- SVG map
-          div
-            [ style "position" "relative"
-            , style "width" "100%"
-            , style "height" "50vh"
-            ]
-            [ viewMap (activeMap model) [] model ]
-
-        -- optional: keep the JSON panel for debugging
-        , div [ style "padding" "8px" ]
-            [ h4 [ style "margin" "0 0 6px 0", style "font-family" "system-ui" ] [ text "FedWiki page JSON" ]
-            , if String.isEmpty model.fedWikiRaw then
-                div [ style "color" "#666", style "font-family" "system-ui", style "font-size" "12px" ]
-                    [ text "No page JSON received yet." ]
-
-              else
-                pre
-                    [ style "white-space" "pre-wrap"
-                    , style "word-break" "break-word"
-                    , style "font-family" "ui-monospace, SFMono-Regular, Menlo, Consolas, monospace"
-                    , style "font-size" "12px"
-                    , style "border" "1px solid #eee"
-                    , style "padding" "8px"
-                    , style "border-radius" "6px"
-                    , style "background" "#fafafa"
-                    ]
-                    [ text model.fedWikiRaw ]
+    Browser.Document
+        "DM6 Elm"
+        [ div
+            (mouseHoverHandler
+                ++ appStyle
+            )
+            ([ viewToolbar model
+             , viewMap (activeMap model) [] model -- mapPath = []
+             ]
+                ++ viewResultMenu model
+                ++ viewIconMenu model
+            )
+        , div
+            ([ id "measure" ]
+                ++ measureStyle
+            )
+            [ text model.measureText
+            , br [] []
             ]
         ]
-    }
+
+
+appStyle : List (Attribute Msg)
+appStyle =
+    [ style "font-family" mainFont
+    , style "user-select" "none"
+    , style "-webkit-user-select" "none" -- Safari still needs vendor prefix
+    ]
+
+
+measureStyle : List (Attribute Msg)
+measureStyle =
+    [ style "position" "fixed"
+    , style "visibility" "hidden"
+    , style "white-space" "pre-wrap"
+    , style "font-family" mainFont
+    , style "font-size" <| fromInt contentFontSize ++ "px"
+    , style "line-height" <| fromFloat topicLineHeight
+    , style "padding" <| fromInt topicDetailPadding ++ "px"
+    , style "width" <| fromFloat topicDetailMaxWidth ++ "px"
+    , style "min-width" <| fromFloat (topicSize.w - topicSize.h) ++ "px"
+    , style "max-width" "max-content"
+    , style "border-width" <| fromFloat topicBorderWidth ++ "px"
+    , style "border-style" "solid"
+    , style "box-sizing" "border-box"
+    ]
 
 
 
--- MAIN
+-- UPDATE
 
 
-main : Program Flags Model Msg
-main =
-    Browser.document
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
-
-
-{-| Compatibility wrapper used by Feature.OpenDoor.\* and tests.
-
-    moveTopicToMap topicId fromId origPos targetId parentPath pos model
-
--}
-moveTopicToMap :
-    Id
-    -> Id
-    -> Point
-    -> Id
-    -> List Id
-    -> Point
-    -> Model
-    -> Model
-moveTopicToMap topicId fromId origPos targetId parentPath pos model =
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
     let
-        fromBoundary =
-            if fromId == 0 then
-                Channel.Root
+        _ =
+            case msg of
+                Mouse _ ->
+                    msg
 
-            else
-                Channel.Container fromId
-
-        toBoundary =
-            if targetId == 0 then
-                Channel.Root
-
-            else
-                Channel.Container targetId
-
-        req =
-            { topicId = topicId
-            , from = fromBoundary
-            , to = toBoundary
-            , pos = pos
-            , permit = Channel.defaultPermit
-            }
+                _ ->
+                    info "update" msg
     in
-    case Channel.cross req model of
-        Ok ( model1, _, _ ) ->
-            model1
+    case msg of
+        FedWikiPage raw ->
+            -- store the incoming FedWiki page JSON string
+            ( { model | fedWikiRaw = raw }, Cmd.none )
 
-        Err _ ->
+        AddTopic ->
+            createTopicIn topicDefaultText Nothing [ activeMap model ] model |> storeModel
+
+        MoveTopicToMap topicId mapId origPos targetId targetMapPath pos ->
+            moveTopicToMap topicId mapId origPos targetId targetMapPath pos model |> storeModel
+
+        SwitchDisplay displayMode ->
+            switchDisplay displayMode model |> storeModel
+
+        Search searchMsg ->
+            updateSearch searchMsg model
+
+        Edit editMsg ->
+            updateEdit editMsg model
+
+        IconMenu iconMenuMsg ->
+            updateIconMenu iconMenuMsg model
+
+        Mouse mouseMsg ->
+            updateMouse mouseMsg model
+
+        Nav navMsg ->
+            updateNav navMsg model |> storeModel
+
+        Hide ->
+            hide model |> storeModel
+
+        Delete ->
+            delete model |> storeModel
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- Derive targetMapId from the path; upstream createMapIfNeeded takes 2 args
+
+
+moveTopicToMap : Id -> MapId -> Point -> Id -> List MapId -> Point -> Model -> Model
+moveTopicToMap topicId containerId origPos targetId targetMapPath newPos model0 =
+    let
+        targetMapId =
+            targetMapPath |> List.reverse |> List.head |> Maybe.withDefault 0
+
+        isSelfTarget =
+            targetId == topicId
+
+        ( model1, created ) =
+            createMapIfNeeded targetId model0
+
+        actualPos =
+            if created then
+                Point (topicW2 + whiteBoxPadding) (topicH2 + whiteBoxPadding)
+
+            else
+                newPos
+
+        props_ =
+            getTopicProps topicId containerId model1.maps
+                |> Maybe.map (\p -> MapTopic { p | pos = actualPos })
+    in
+    if isSelfTarget then
+        model0
+
+    else
+        case props_ of
+            Just props ->
+                model1
+                    |> hideItem topicId containerId
+                    |> setTopicPos topicId containerId origPos
+                    |> addItemToMap topicId props targetId
+                    |> select targetId targetMapPath
+                    |> autoSize
+
+            Nothing ->
+                model0
+
+
+createMapIfNeeded : Id -> Model -> ( Model, Bool )
+createMapIfNeeded topicId model =
+    if hasMap topicId model.maps then
+        ( model, False )
+
+    else
+        ( model
+            |> createMap topicId
+            |> setDisplayModeInAllMaps topicId (Container BlackBox)
+          -- A nested topic which becomes a container might exist in other maps as well, still as
+          -- a monad. We must set the topic's display mode to "container" in *all* maps. Otherwise
+          -- in the other maps it might be revealed still as a monad.
+        , True
+        )
+
+
+setDisplayModeInAllMaps : Id -> DisplayMode -> Model -> Model
+setDisplayModeInAllMaps topicId displayMode model =
+    model.maps
+        |> Dict.foldr
+            (\mapId _ modelAcc ->
+                case isItemInMap topicId mapId model of
+                    True ->
+                        setDisplayMode topicId mapId displayMode modelAcc
+
+                    False ->
+                        modelAcc
+            )
             model
+
+
+switchDisplay : DisplayMode -> Model -> Model
+switchDisplay displayMode model =
+    (case getSingleSelection model of
+        Just ( containerId, mapPath ) ->
+            let
+                mapId =
+                    getMapId mapPath
+            in
+            { model
+                | maps =
+                    case displayMode of
+                        Monad _ ->
+                            model.maps
+
+                        Container BlackBox ->
+                            boxContainer containerId mapId model
+
+                        Container WhiteBox ->
+                            boxContainer containerId mapId model
+
+                        Container Unboxed ->
+                            unboxContainer containerId mapId model
+            }
+                |> setDisplayMode containerId mapId displayMode
+
+        Nothing ->
+            model
+    )
+        |> autoSize
+
+
+
+-- Text Edit
+
+
+updateEdit : EditMsg -> Model -> ( Model, Cmd Msg )
+updateEdit msg model =
+    case msg of
+        EditStart ->
+            startEdit model
+
+        OnTextInput text ->
+            onTextInput text model |> storeModel
+
+        OnTextareaInput text ->
+            onTextareaInput text model |> storeModelWith
+
+        SetTopicSize topicId mapId size ->
+            ( model
+                |> setTopicSize topicId mapId size
+                |> autoSize
+            , Cmd.none
+            )
+
+        EditEnd ->
+            ( endEdit model, Cmd.none )
+
+
+startEdit : Model -> ( Model, Cmd Msg )
+startEdit model =
+    let
+        newModel =
+            case getSingleSelection model of
+                Just ( topicId, mapPath ) ->
+                    { model | editState = ItemEdit topicId (getMapId mapPath) }
+                        |> setDetailDisplayIfMonade topicId (getMapId mapPath)
+                        |> autoSize
+
+                Nothing ->
+                    model
+    in
+    ( newModel, focus newModel )
+
+
+setDetailDisplayIfMonade : Id -> MapId -> Model -> Model
+setDetailDisplayIfMonade topicId mapId model =
+    model
+        |> updateTopicProps topicId
+            mapId
+            (\props ->
+                case props.displayMode of
+                    Monad _ ->
+                        { props | displayMode = Monad Detail }
+
+                    _ ->
+                        props
+            )
+
+
+onTextInput : String -> Model -> Model
+onTextInput text model =
+    case model.editState of
+        ItemEdit topicId _ ->
+            updateTopicInfo topicId
+                (\topic -> { topic | text = text })
+                model
+
+        NoEdit ->
+            logError "onTextInput" "called when editState is NoEdit" model
+
+
+onTextareaInput : String -> Model -> ( Model, Cmd Msg )
+onTextareaInput text model =
+    case model.editState of
+        ItemEdit topicId mapId ->
+            updateTopicInfo topicId
+                (\topic -> { topic | text = text })
+                model
+                |> measureText text topicId mapId
+
+        NoEdit ->
+            logError "onTextareaInput" "called when editState is NoEdit" ( model, Cmd.none )
+
+
+measureText : String -> Id -> MapId -> Model -> ( Model, Cmd Msg )
+measureText text topicId mapId model =
+    ( { model | measureText = text }
+    , Dom.getElement "measure"
+        |> Task.attempt
+            (\result ->
+                case result of
+                    Ok elem ->
+                        Edit
+                            (SetTopicSize topicId
+                                mapId
+                                (Size elem.element.width elem.element.height)
+                            )
+
+                    Err err ->
+                        logError "measureText" (toString err) NoOp
+            )
+    )
+
+
+endEdit : Model -> Model
+endEdit model =
+    { model | editState = NoEdit }
+        |> autoSize
+
+
+focus : Model -> Cmd Msg
+focus model =
+    let
+        nodeId =
+            case model.editState of
+                ItemEdit id mapId ->
+                    "dmx-input-" ++ fromInt id ++ "-" ++ fromInt mapId
+
+                NoEdit ->
+                    logError "focus" "called when editState is NoEdit" ""
+    in
+    Dom.focus nodeId
+        |> Task.attempt
+            (\result ->
+                case result of
+                    Ok () ->
+                        NoOp
+
+                    Err e ->
+                        logError "focus" (toString e) NoOp
+            )
+
+
+
+--
+
+
+updateNav : NavMsg -> Model -> Model
+updateNav navMsg model =
+    case navMsg of
+        Fullscreen ->
+            fullscreen model
+
+        Back ->
+            back model
+
+
+fullscreen : Model -> Model
+fullscreen model =
+    case getSingleSelection model of
+        Just ( topicId, _ ) ->
+            { model
+                | mapPath = topicId :: model.mapPath
+                , selection = []
+            }
+                |> createMapIfNeeded topicId
+                |> Tuple.first
+                |> adjustMapRect topicId -1
+
+        Nothing ->
+            model
+
+
+back : Model -> Model
+back model =
+    let
+        ( mapId, mapPath, selection ) =
+            case model.mapPath of
+                prevMapId :: nextMapId :: mapIds ->
+                    ( prevMapId
+                    , nextMapId :: mapIds
+                    , [ ( prevMapId, nextMapId ) ]
+                    )
+
+                _ ->
+                    logError "back" "model.mapPath has a problem" ( 0, [ 0 ], [] )
+    in
+    { model
+        | mapPath = mapPath
+
+        -- , selection = selection -- TODO
+    }
+        |> adjustMapRect mapId 1
+        |> autoSize
+
+
+adjustMapRect : MapId -> Float -> Model -> Model
+adjustMapRect mapId factor model =
+    model
+        |> updateMapRect mapId
+            (\rect ->
+                Rectangle
+                    (rect.x1 + factor * 400)
+                    -- TODO
+                    (rect.y1 + factor * 300)
+                    -- TODO
+                    rect.x2
+                    rect.y2
+            )
+
+
+hide : Model -> Model
+hide model =
+    let
+        newModel =
+            model.selection
+                |> List.foldr
+                    (\( itemId, mapPath ) modelAcc -> hideItem itemId (getMapId mapPath) modelAcc)
+                    model
+    in
+    { newModel | selection = [] }
+        |> autoSize
+
+
+delete : Model -> Model
+delete model =
+    let
+        newModel =
+            model.selection
+                |> List.map Tuple.first
+                |> List.foldr
+                    (\itemId modelAcc -> deleteItem itemId modelAcc)
+                    model
+    in
+    { newModel | selection = [] }
+        |> autoSize
