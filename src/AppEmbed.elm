dc10ac9fd5ecb55ed40edfa6f9e5ea3455b0c113
RalfBarkow <ralf.barkow@me.com>
2025-09-16 15:27:49 +0200

    feat(embed): simplify AppEmbed by delegating to AppRunner
    * remove unused Compat.FedWiki and storedDecoder
    * drop custom init/update/subscriptions, reuse App.init/App.update
    * wire subscriptions directly with App.subscriptions and pageJson App.FedWikiPage
    * switch Html import to alias H for consistency

--- src/AppEmbed.elm
+++ src/AppEmbed.elm
@@ -1,12 +1,11 @@
 port module AppEmbed exposing (main)
 
-import AppModel as AM
-import AppRunner as App exposing (Msg(..), UndoModel)
+import AppRunner as App exposing (Msg(..))
 import Browser
-import Compat.FedWiki as CFW
-import Html exposing (Html)
-import Json.Decode as D
+import Html as H
 import Json.Encode as E
+import Model exposing (..)
+import Platform.Sub as Sub
 
 
 
@@ -16,44 +15,21 @@ import Json.Encode as E
 port pageJson : (String -> msg) -> Sub msg
 
 
-storedDecoder : D.Decoder String
-storedDecoder =
-    D.oneOf
-        [ D.field "stored" D.string -- your cold-boot.html passes this
-        , D.field "pageJson" D.string -- future-proof alias
-        , D.succeed "{}"
-        ]
-
-
-init : E.Value -> ( App.UndoModel, Cmd App.Msg )
-init flagsValue =
-    -- just delegate to App.init, do NOT decode/import flags.stored here
-    App.init flagsValue
-
-
-update : App.Msg -> App.UndoModel -> ( App.UndoModel, Cmd App.Msg )
-update =
-    App.update
-
-
-view : App.UndoModel -> Html App.Msg
+view : App.UndoModel -> H.Html App.Msg
 view =
     App.view
-
-
-subscriptions : App.UndoModel -> Sub App.Msg
-subscriptions undo =
-    Sub.batch
-        [ App.subscriptions undo -- Sub App.Msg
-        , pageJson FedWikiPage -- Sub App.Msg
-        ]
 
 
 main : Program E.Value App.UndoModel App.Msg
 main =
     Browser.element
-        { init = init
+        { init = App.init
+        , update = App.update
+        , subscriptions =
+            \undo ->
+                Sub.batch
+                    [ App.subscriptions undo
+                    , pageJson App.FedWikiPage
+                    ]
         , view = view
-        , update = update
-        , subscriptions = subscriptions
         }
