2d0989640e6315e254bb8852c20dd0b8b2c43441
RalfBarkow <ralf.barkow@me.com>
2025-09-30 08:40:01 +0200

    feat(svg): add SvgExtras module with pointer-events and cursor helpers

---
+++ src/SvgExtras.elm
@@ -0,0 +1,20 @@
+module SvgExtras exposing (cursorPointer, peAll, peNone, peStroke)
+
+import Svg exposing (Attribute)
+import Svg.Attributes as SA
+
+peNone : Attribute msg
+peNone =
+    SA.style "pointer-events: none"
+
+peStroke : Attribute msg
+peStroke =
+    SA.style "pointer-events: visibleStroke"
+
+peAll : Attribute msg
+peAll =
+    SA.style "pointer-events: all"
+
+cursorPointer : Attribute msg
+cursorPointer =
+    SA.style "cursor: pointer"
