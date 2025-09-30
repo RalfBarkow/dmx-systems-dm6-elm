7e0a3836b254ac736dca68e41312e3c77c1e0453
RalfBarkow <ralf.barkow@me.com>
2025-09-30 09:50:57 +0200

    feat(svg): add selectable circles demo using pointer-events and
    Svg.Events in FedWiki container
    
    Replace Main.elm with a minimal Browser.sandbox demo
    showcasing three clickable circles inside a FedWiki-like SVG container.
    The container background uses `peNone` so it doesn’t intercept clicks;
    circles use `peAll`, `cursorPointer`, and `Svg.Events.onClick` to
    toggle selection.
    
    Also tidy `SvgExtras.elm` (no behavioral change) and export helpers
    (`peNone`, `peAll`, `peStroke`, `cursorPointer`) for reuse.
    
    BREAKING CHANGE: Main module is now a demo entrypoint;
    previous application logic (maps, undo/redo, search, etc.) is
    removed from Main.

--- src/Main.elm
+++ src/Main.elm
@@ -1,593 +1,126 @@
-module Main exposing (..)
+module Main exposing (main)
 
--- components
-
-import AppModel exposing (..)
-import Boxing exposing (boxContainer, unboxContainer)
 import Browser
-import Browser.Dom as Dom
-import Compat.Model as CModel
-import Config exposing (..)
-import Dict
-import Html exposing (Attribute, br, div, text)
-import Html.Attributes exposing (id, style)
-import IconMenuAPI exposing (updateIconMenu, viewIconMenu)
-import Json.Decode as D
-import Json.Encode as E
-import MapAutoSize exposing (autoSize)
-import MapRenderer exposing (viewMap)
-import Model as M exposing (..)
-import ModelAPI exposing (..)
-import MouseAPI exposing (mouseHoverHandler, mouseSubs, updateMouse)
-import SearchAPI exposing (updateSearch, viewResultMenu)
-import Storage exposing (exportJSON, importJSON, modelDecoder, store, storeWith)
-import String exposing (fromFloat, fromInt)
-import Task
-import Toolbar exposing (viewToolbar)
-import UndoList
-import Utils exposing (..)
+import Html exposing (Html, div, text)
+import Svg exposing (Svg, circle, rect, svg)
+import Svg.Attributes as SA
+import Svg.Events as SE
+import SvgExtras exposing (cursorPointer, peAll, peNone)
 
 
 
--- MAIN
+-- MODEL
 
 
-main : Program E.Value UndoModel Msg
-main =
-    Browser.document
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = mouseSubs
-        }
+type alias Model =
+    { selectedCircle : Maybe String }
 
 
-init : E.Value -> ( UndoModel, Cmd Msg )
-init flags =
-    ( initModel flags, Cmd.none ) |> reset
-
-
-initModel : E.Value -> Model
-initModel flags =
-    case flags |> D.decodeValue (D.null True) of
-        Ok True ->
-            let
-                _ =
-                    info "init" "localStorage: empty"
-            in
-            AppModel.default |> ensureCurrentMap
-
-        _ ->
-            case flags |> D.decodeValue modelDecoder of
-                Ok model ->
-                    let
-                        _ =
-                            info "init"
-                                ("localStorage: " ++ (model |> toString |> String.length |> fromInt) ++ " bytes")
-                    in
-                    ensureCurrentMap model
-
-                Err e ->
-                    let
-                        _ =
-                            logError "init" "localStorage" e
-                    in
-                    AppModel.default |> ensureCurrentMap
-
-
-type alias Id =
-    Int
-
-
-blankRect : M.Rectangle
-blankRect =
-    { x1 = 0, y1 = 0, x2 = 0, y2 = 0 }
-
-
-
--- adjust field names if your alias differs
-
-
-emptyItems : M.MapItems
-emptyItems =
-    Dict.empty
-
-
-mkRoot : M.MapId -> M.Map
-mkRoot rid =
-    CModel.makeMapR { id = rid, rect = blankRect, items = emptyItems }
-
-
-ensureRootOnInit : Model -> Model
-ensureRootOnInit model0 =
-    if Dict.isEmpty model0.maps then
-        let
-            rid =
-                model0.nextId
-
-            root =
-                mkRoot rid
-        in
-        { model0
-            | maps = Dict.insert rid root model0.maps
-            , mapPath = [ rid ]
-            , nextId = rid + 1
-        }
-
-    else
-        -- also make sure mapPath head points to an existing map
-        ensureCurrentMap model0
-
-
-
--- VIEW
-
-
-view : UndoModel -> Browser.Document Msg
-view ({ present } as undoModel) =
-    Browser.Document
-        "DM6 Elm"
-        [ div
-            (mouseHoverHandler
-                ++ appStyle
-            )
-            ([ viewToolbar undoModel
-             , viewMap (activeMap present) [] present -- mapPath = []
-             ]
-                ++ viewResultMenu present
-                ++ viewIconMenu present
-            )
-        , div
-            (id "measure" :: measureStyle)
-            [ text present.measureText
-            , br [] []
-            ]
-        ]
-
-
-appStyle : List (Attribute Msg)
-appStyle =
-    [ style "font-family" mainFont
-    , style "user-select" "none"
-    , style "-webkit-user-select" "none" -- Safari still needs vendor prefix
-    ]
-
-
-measureStyle : List (Attribute Msg)
-measureStyle =
-    [ style "position" "fixed"
-    , style "visibility" "hidden"
-    , style "white-space" "pre-wrap"
-    , style "font-family" mainFont
-    , style "font-size" <| fromInt contentFontSize ++ "px"
-    , style "line-height" <| fromFloat topicLineHeight
-    , style "padding" <| fromInt topicDetailPadding ++ "px"
-    , style "width" <| fromFloat topicDetailMaxWidth ++ "px"
-    , style "min-width" <| fromFloat (topicSize.w - topicSize.h) ++ "px"
-    , style "max-width" "max-content"
-    , style "border-width" <| fromFloat topicBorderWidth ++ "px"
-    , style "border-style" "solid"
-    , style "box-sizing" "border-box"
-    ]
+init : Model
+init =
+    { selectedCircle = Nothing }
 
 
 
 -- UPDATE
 
 
-update : Msg -> UndoModel -> ( UndoModel, Cmd Msg )
-update msg ({ present } as undoModel) =
-    let
-        _ =
-            case msg of
-                Mouse _ ->
-                    msg
+type Msg
+    = SelectCircle String
 
-                _ ->
-                    info "update" msg
-    in
+
+update : Msg -> Model -> Model
+update msg model =
     case msg of
-        AddTopic ->
-            createTopicIn topicDefaultText Nothing [ activeMap present ] present
-                |> store
-                |> push undoModel
+        SelectCircle circleId ->
+            { model
+                | selectedCircle =
+                    if model.selectedCircle == Just circleId then
+                        Nothing
 
-        MoveTopicToMap topicId mapId origPos targetId targetMapPath pos ->
-            moveTopicToMap topicId mapId origPos targetId targetMapPath pos present
-                |> store
-                |> push undoModel
-
-        SwitchDisplay displayMode ->
-            switchDisplay displayMode present
-                |> store
-                |> swap undoModel
-
-        Search searchMsg ->
-            updateSearch searchMsg undoModel
-
-        Edit editMsg ->
-            updateEdit editMsg undoModel
-
-        IconMenu iconMenuMsg ->
-            updateIconMenu iconMenuMsg undoModel
-
-        Mouse mouseMsg ->
-            updateMouse mouseMsg undoModel
-
-        Nav navMsg ->
-            updateNav navMsg present |> store |> reset
-
-        Hide ->
-            hide present |> store |> push undoModel
-
-        Delete ->
-            delete present |> store |> push undoModel
-
-        Undo ->
-            undo undoModel
-
-        Redo ->
-            redo undoModel
-
-        Import ->
-            ( present, importJSON () ) |> swap undoModel
-
-        Export ->
-            ( present, exportJSON () ) |> swap undoModel
-
-        NoOp ->
-            ( present, Cmd.none ) |> swap undoModel
+                    else
+                        Just circleId
+            }
 
 
-moveTopicToMap : Id -> MapId -> Point -> Id -> MapPath -> Point -> Model -> Model
-moveTopicToMap topicId mapId origPos targetId targetMapPath pos model =
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text "Click circles to select/deselect" ]
+        , div [] [ text <| "Selected: " ++ (model.selectedCircle |> Maybe.withDefault "None") ]
+        , div [ SA.class "fedwiki-container" ]
+            [ svg
+                [ SA.width "100%"
+                , SA.height "400px"
+                , SA.viewBox "0 0 800 600"
+                ]
+                [ -- FedWiki container
+                  rect
+                    [ SA.x "50"
+                    , SA.y "50"
+                    , SA.width "700"
+                    , SA.height "500"
+                    , SA.fill "#f8f8f8"
+                    , SA.stroke "#ddd"
+                    , SA.strokeWidth "2"
+                    , peNone -- Background doesn't intercept clicks
+                    ]
+                    []
+
+                -- Selectable circles
+                , viewCircle "circle1" 200 200 model
+                , viewCircle "circle2" 400 200 model
+                , viewCircle "circle3" 600 200 model
+                ]
+            ]
+        ]
+
+
+viewCircle : String -> Float -> Float -> Model -> Svg Msg
+viewCircle circleId cx cy model =
     let
-        ( newModel, created ) =
-            createMapIfNeeded targetId model
+        isSelected =
+            model.selectedCircle == Just circleId
 
-        newPos =
-            if created then
-                Point
-                    (topicW2 + whiteBoxPadding)
-                    (topicH2 + whiteBoxPadding)
+        fillColor =
+            if isSelected then
+                "#4CAF50"
 
             else
-                pos
-
-        props_ =
-            getTopicProps topicId mapId newModel.maps
-                |> Maybe.andThen (\props -> Just (MapTopic { props | pos = newPos }))
+                "#2196F3"
     in
-    case props_ of
-        Just props ->
-            newModel
-                |> hideItem topicId mapId
-                |> setTopicPos topicId mapId origPos
-                |> addItemToMap topicId props targetId
-                |> select targetId targetMapPath
-                |> autoSize
+    circle
+        [ SA.cx (String.fromFloat cx)
+        , SA.cy (String.fromFloat cy)
+        , SA.r "40"
+        , SA.fill fillColor
+        , SA.stroke "#333"
+        , SA.strokeWidth
+            (if isSelected then
+                "3"
 
-        Nothing ->
-            model
-
-
-createMapIfNeeded : Id -> Model -> ( Model, Bool )
-createMapIfNeeded topicId model =
-    if hasMap topicId model.maps then
-        ( model, False )
-
-    else
-        ( model
-            |> createMap topicId
-            |> setDisplayModeInAllMaps topicId (Container BlackBox)
-          -- A nested topic which becomes a container might exist in other maps as well, still as
-          -- a monad. We must set the topic's display mode to "container" in *all* maps. Otherwise
-          -- in the other maps it might be revealed still as a monad.
-        , True
-        )
-
-
-setDisplayModeInAllMaps : Id -> DisplayMode -> Model -> Model
-setDisplayModeInAllMaps topicId displayMode model =
-    model.maps
-        |> Dict.foldr
-            (\mapId _ modelAcc ->
-                if isItemInMap topicId mapId model then
-                    setDisplayMode topicId mapId displayMode modelAcc
-
-                else
-                    modelAcc
+             else
+                "1"
             )
-            model
+        , peAll
+        , cursorPointer
+        , SE.onClick (SelectCircle circleId)
+        ]
+        []
 
 
-switchDisplay : DisplayMode -> Model -> Model
-switchDisplay displayMode model =
-    (case getSingleSelection model of
-        Just ( containerId, mapPath ) ->
-            let
-                mapId =
-                    getMapId mapPath
-            in
-            { model
-                | maps =
-                    case displayMode of
-                        Monad _ ->
-                            model.maps
 
-                        Container BlackBox ->
-                            boxContainer containerId mapId model
+-- MAIN
 
-                        Container WhiteBox ->
-                            boxContainer containerId mapId model
 
-                        Container Unboxed ->
-                            unboxContainer containerId mapId model
-            }
-                |> setDisplayMode containerId mapId displayMode
-
-        Nothing ->
-            model
-    )
-        |> autoSize
-
-
-
--- Text Edit
-
-
-updateEdit : EditMsg -> UndoModel -> ( UndoModel, Cmd Msg )
-updateEdit msg ({ present } as undoModel) =
-    case msg of
-        EditStart ->
-            startEdit present |> push undoModel
-
-        OnTextInput text ->
-            onTextInput text present |> store |> swap undoModel
-
-        OnTextareaInput text ->
-            onTextareaInput text present |> storeWith |> swap undoModel
-
-        SetTopicSize topicId mapId size ->
-            ( present
-                |> setTopicSize topicId mapId size
-                |> autoSize
-            , Cmd.none
-            )
-                |> swap undoModel
-
-        EditEnd ->
-            ( endEdit present, Cmd.none )
-                |> swap undoModel
-
-
-startEdit : Model -> ( Model, Cmd Msg )
-startEdit model =
-    let
-        newModel =
-            case getSingleSelection model of
-                Just ( topicId, mapPath ) ->
-                    { model | editState = ItemEdit topicId (getMapId mapPath) }
-                        |> setDetailDisplayIfMonade topicId (getMapId mapPath)
-                        |> autoSize
-
-                Nothing ->
-                    model
-    in
-    ( newModel, focus newModel )
-
-
-setDetailDisplayIfMonade : Id -> MapId -> Model -> Model
-setDetailDisplayIfMonade topicId mapId model =
-    model
-        |> updateTopicProps topicId
-            mapId
-            (\props ->
-                case props.displayMode of
-                    Monad _ ->
-                        { props | displayMode = Monad Detail }
-
-                    _ ->
-                        props
-            )
-
-
-onTextInput : String -> Model -> Model
-onTextInput text model =
-    case model.editState of
-        ItemEdit topicId _ ->
-            updateTopicInfo topicId
-                (\topic -> { topic | text = text })
-                model
-
-        NoEdit ->
-            logError "onTextInput" "called when editState is NoEdit" model
-
-
-onTextareaInput : String -> Model -> ( Model, Cmd Msg )
-onTextareaInput text model =
-    case model.editState of
-        ItemEdit topicId mapId ->
-            updateTopicInfo topicId
-                (\topic -> { topic | text = text })
-                model
-                |> measureText text topicId mapId
-
-        NoEdit ->
-            logError "onTextareaInput" "called when editState is NoEdit" ( model, Cmd.none )
-
-
-measureText : String -> Id -> MapId -> Model -> ( Model, Cmd Msg )
-measureText text topicId mapId model =
-    ( { model | measureText = text }
-    , Dom.getElement "measure"
-        |> Task.attempt
-            (\result ->
-                case result of
-                    Ok elem ->
-                        Edit
-                            (SetTopicSize topicId
-                                mapId
-                                (Size elem.element.width elem.element.height)
-                            )
-
-                    Err err ->
-                        logError "measureText" (toString err) NoOp
-            )
-    )
-
-
-endEdit : Model -> Model
-endEdit model =
-    { model | editState = NoEdit }
-        |> autoSize
-
-
-focus : Model -> Cmd Msg
-focus model =
-    let
-        nodeId =
-            case model.editState of
-                ItemEdit id mapId ->
-                    "dmx-input-" ++ fromInt id ++ "-" ++ fromInt mapId
-
-                NoEdit ->
-                    logError "focus" "called when editState is NoEdit" ""
-    in
-    Dom.focus nodeId
-        |> Task.attempt
-            (\result ->
-                case result of
-                    Ok () ->
-                        NoOp
-
-                    Err e ->
-                        logError "focus" (toString e) NoOp
-            )
-
-
-
---
-
-
-updateNav : NavMsg -> Model -> Model
-updateNav navMsg model =
-    case navMsg of
-        Fullscreen ->
-            fullscreen model
-
-        Back ->
-            back model
-
-
-fullscreen : Model -> Model
-fullscreen model =
-    case getSingleSelection model of
-        Just ( topicId, _ ) ->
-            { model | mapPath = topicId :: model.mapPath }
-                |> resetSelection
-                |> createMapIfNeeded topicId
-                |> Tuple.first
-                |> adjustMapRect topicId -1
-
-        Nothing ->
-            model
-
-
-back : Model -> Model
-back model =
-    let
-        ( mapId, mapPath, _ ) =
-            case model.mapPath of
-                prevMapId :: nextMapId :: mapIds ->
-                    ( prevMapId
-                    , nextMapId :: mapIds
-                    , [ ( prevMapId, nextMapId ) ]
-                    )
-
-                _ ->
-                    logError "back" "model.mapPath has a problem" ( 0, [ 0 ], [] )
-    in
-    { model
-        | mapPath = mapPath
-
-        -- , selection = selection -- TODO
-    }
-        |> adjustMapRect mapId 1
-        |> autoSize
-
-
-adjustMapRect : MapId -> Float -> Model -> Model
-adjustMapRect mapId factor model =
-    model
-        |> updateMapRect mapId
-            (\rect ->
-                Rectangle
-                    (rect.x1 + factor * 400)
-                    -- TODO
-                    (rect.y1 + factor * 300)
-                    -- TODO
-                    rect.x2
-                    rect.y2
-            )
-
-
-hide : Model -> Model
-hide model =
-    let
-        newModel =
-            model.selection
-                |> List.foldr
-                    (\( itemId, mapPath ) modelAcc -> hideItem itemId (getMapId mapPath) modelAcc)
-                    model
-    in
-    newModel
-        |> resetSelection
-        |> autoSize
-
-
-delete : Model -> Model
-delete model =
-    let
-        newModel =
-            model.selection
-                |> List.map Tuple.first
-                |> List.foldr
-                    (\itemId modelAcc -> deleteItem itemId modelAcc)
-                    model
-    in
-    newModel
-        |> resetSelection
-        |> autoSize
-
-
-
--- Undo / Redo
-
-
-undo : UndoModel -> ( UndoModel, Cmd Msg )
-undo undoModel =
-    let
-        newUndoModel =
-            UndoList.undo undoModel
-
-        newModel =
-            resetTransientState newUndoModel.present
-    in
-    newModel
-        |> store
-        |> swap newUndoModel
-
-
-redo : UndoModel -> ( UndoModel, Cmd Msg )
-redo undoModel =
-    let
-        newUndoModel =
-            UndoList.redo undoModel
-
-        newModel =
-            resetTransientState newUndoModel.present
-    in
-    newModel
-        |> store
-        |> swap newUndoModel
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
--- src/SvgExtras.elm
+++ src/SvgExtras.elm
@@ -3,17 +3,21 @@ module SvgExtras exposing (cursorPointer, peAll, peNone, peStroke)
 import Svg exposing (Attribute)
 import Svg.Attributes as SA
 
+
 peNone : Attribute msg
 peNone =
     SA.style "pointer-events: none"
+
 
 peStroke : Attribute msg
 peStroke =
     SA.style "pointer-events: visibleStroke"
 
+
 peAll : Attribute msg
 peAll =
     SA.style "pointer-events: all"
+
 
 cursorPointer : Attribute msg
 cursorPointer =
